e7e075df43e755b8c4f3a15cebed4f7a2266d8c0
k <k@civiland.io>
2022-04-25 00:10:31 +0800

    clean

--- contracts/token/IERC721C.sol
+++
@@ -1,5 +0,0 @@
-interface IERC721C {
-    function getLayerCount() external view returns(uint8);
-    function poolMint(address to) external returns(uint256);
-    function burn(address tokenOwner, uint256 tokenId) external;
-}
